956e6332a91a8219556f7310f90bdd371420530e
Stephen Brobak <Stephen.Brobak@bellevuecollege.edu>
2015-08-11 11:02:56 +0200

    Created the joomla test project

---
+++ JoomlaTest/Classes/MyClass.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brobak
+ * Date: 04-08-2015
+ * Time: 13:13
+ */
+
+namespace mySpace;
+
+
+class MyClass
+{
+    function sum($x, $y){
+        return ($x + $y);
+    }
+
+    function multiply($x, $y){
+        return ($x * $y);
+    }
+
+    function half($x){
+        return ($x / 2);
+    }
+
+    function double($x){
+        return ($x * 2);
+    }
+}
