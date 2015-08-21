7cd72281a915593dffa1ed7aca0e396d48677e1a
Stephen Brobak <Stephen.Brobak@bellevuecollege.edu>
2015-08-21 09:49:54 +0200

    m

--- build/tests/UnitTests/MyClassSumTest.php
+++ build/tests/UnitTests/MyClassSumTest.php
@@ -7,7 +7,7 @@
  */
 
 namespace mySpace;
-require_once(__DIR__."/../../build/src/JoomlaTest/Classes/MyClass.php");
+require_once(__DIR__."/../../../build/src/JoomlaTest/Classes/MyClass.php");
 
 class MyClassSumTest extends \PHPUnit_Framework_TestCase
 {
